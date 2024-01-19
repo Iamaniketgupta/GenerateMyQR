87b55aa850e3617ef234b294309c527d0b50226b
Aniket Gupta <aniketgupta.org@gmail.com>
2024-01-19 14:27:30 +0530

    updated

--- Assets/script.js
+++ Assets/script.js
@@ -1,18 +1,28 @@
-const themeBtn =document.querySelector("#theme-changer i");
-const body=document.querySelector("body");
-const nav=document.querySelector("#nav-wrapper");
-const navIconBars=document.querySelector(".fa-bars");
-const navIconCross=document.querySelector("#cross");
+const themeBtn = document.querySelector("#theme-changer i");
+const body = document.querySelector("body");
+const nav = document.querySelector("#nav-wrapper");
+const navIconBars = document.querySelector(".fa-bars");
+const navIconCross = document.querySelector("#cross");
+const qrForm = document.getElementById("qr-form");
+const qrInputBox = document.getElementById("qr_url");
+const qrSize = document.getElementById("size");
+const qrCodeContainer = document.getElementById("qr-code");
+const loader = document.querySelector("#loader");
+const colorVal = document.querySelector("#color-picker");
+const downloadBtn = document.querySelector("#download-qr");
 
-//default theme
-body.classList.add("dark-theme");
-themeBtn.addEventListener("click",(e)=>{
+colorVal.value="#000000"; //default color
 
-    if(e.target.classList.contains("fa-sun")){
+body.classList.add("dark-theme"); // default theme
+
+//  THEME
+themeBtn.addEventListener("click", (e) => {
+
+    if (e.target.classList.contains("fa-sun")) {
         body.classList.remove("dark-theme");
         body.classList.add("light-theme");
     }
-    else{
+    else {
         body.classList.add("dark-theme");
         body.classList.remove("light-theme");
     }
@@ -21,16 +31,66 @@ themeBtn.addEventListener("click",(e)=>{
 
 });
 
+//  NAVBAR
 
-navIconBars.addEventListener("click",()=>{
-    console.log("show nav");
+navIconBars.addEventListener("click", () => {
     nav.classList.toggle("shownav")
 });
 
-navIconCross.addEventListener("click",()=>{
-    console.log("close nav");
+navIconCross.addEventListener("click", () => {
     nav.classList.toggle("shownav")
 });
 
 
+// QR FORM
 
+qrForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+
+    try {
+        
+        await generateQR(qrInputBox.value, qrSize.value, colorVal.value); // Pass the values
+        downloadBtn.style.display="block";
+       
+
+    } catch (err) {
+        console.error("Error generating QR code:", err);
+        loader.style.display = "none";
+    }
+
+});
+
+//  QR GENERATION
+   
+const generateQR = (qrVal, qrSizeVal, colorVal) => {
+    qrCodeContainer.innerHTML = "";
+    try {
+        new QRCode(qrCodeContainer, {
+            text: qrVal,
+            width: qrSizeVal,
+            height: qrSizeVal,
+            colorDark: colorVal,
+            colorLight: "white",
+            correctLevel: QRCode.CorrectLevel.H,
+        });
+    } catch (error) {
+        console.error("Error generating QR code:", error);
+    }
+};
+
+//  DOWNLOAD BUTTON
+
+downloadBtn.addEventListener("click", downloadQR);
+
+function downloadQR() {
+    console.log("click");
+    let qrImg = qrCodeContainer.querySelector("img");
+
+    if (qrImg) {
+        const link = document.createElement("a");
+        link.href = qrImg.src;
+        link.download = "qrcode.png";
+        link.click();
+    }
+}
